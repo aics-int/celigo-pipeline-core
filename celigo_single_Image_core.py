4e9fe530a1f15932a652b9e3e448d25611c627c0
BrianW25 <brian.whitney@alleninstitute.org>
2022-01-27 13:45:05 -0800

    Changed from static methods to a constructor. In the constructor temp_dir, working_dir, and the initial copy of the image-path are all defined. Every time this class is used we will have an raw_ image_path so we can generate temp_dir, working_dir and copy the image over to the new working_dir calling it image_path. It doesnt make sense to do this in other methods since it happens each time. I also added a blank filelist to the class which is later defined by calling run_ilastik.

--- celigo_single_Image_core.py
+++ celigo_single_Image_core.py
@@ -17,37 +17,18 @@ class CeligoSingleImageCore:
     2) Cell Profiler Processing
 
     """
+    filelist = Path('') # I am not sure if this is the correct initialzation for this 
 
-    def __init__(self):
+    def __init__(self, raw_image_path):
         self.temp_dir = tempfile.TemporaryDirectory()
         self.working_dir = Path(self.temp_dir.name)
-
-
-    def copy_celigo_image(
-        self,
-        raw_image_path: Path
-    ) -> Path:
-        """
-        PARAMETERS: Takes in a path to a directory (raw_image_path) to copy from path to a directory to 
-        paste to (self.working_dir).
-        
-        FUNCTIONALITY: This method takes an existing image at a given location and clones it to 
-        somewhere else.
-        
-        :return: Returns a path to the cloned image.
-        """
-
         shutil.copyfile(raw_image_path, f'{self.working_dir}/{raw_image_path.name}')
-        return Path(f'{self.working_dir}/{raw_image_path.name}')
- 
+        self.image_path =  Path(f'{self.working_dir}/{raw_image_path.name}')
 
-    def downsample(
-        self,
-        image_path: Path,
-        scale_factor: int
-    ) -> Path:
+
+    def downsample(self, scale_factor: int):
         """
-        PARAMETERS: Takes in a path to an image (image_path) and a scaling factor (scale_factor).
+        PARAMETERS: Takes in a scaling factor (scale_factor).
         
         FUNCTIONALITY: This method takes an existing image and creates a copy of the image scaled by a given 
         quantity/magnification. Ex. 4 --> 1/4 size.
@@ -55,20 +36,15 @@ class CeligoSingleImageCore:
         :return: Returns a path to the resized image.
         """
 
-        image = AICSImage(image_path)
+        image = AICSImage(self.image_path)
         image_rescaled = rescale(image.get_image_data(), 1 / scale_factor, anti_aliasing=False)
-        image_rescaled_path = image_path.parent / f"{image_path.with_suffix('').name}_rescale.ome.tiff"
+        image_rescaled_path = self.image_path.parent / f"{self.image_path.with_suffix('').name}_rescale.ome.tiff"
         OmeTiffWriter.save(image_rescaled, image_rescaled_path, dim_order= image.dims.order)
-        return image_rescaled_path
+        self.image_path = image_rescaled_path
 
 
-    def run_ilastik(
-        self,
-        image_path: Path
-    ) -> Path:
+    def run_ilastik(self):
         """
-        PARAMETERS: Takes in a path to an image (image_path) and an output directory (self.working_dir).
-        
         FUNCTIONALITY: This method takes an existing image either scaled or unscaled and creates a probability 
         map of [I DON'T KNOW]. It does this by creating a bash script, given above parameters, running said script 
         on the slurm cluster in a pre-existing virtual environment and then generating a list of pertinent files
@@ -97,7 +73,7 @@ class CeligoSingleImageCore:
         --project="/allen/aics/microscopy/CellProfiler_4.1.3_Testing/ballingandlifting.ilp" 
         --output_format=tiff 
         --export_source="Probabilities" 
-        --raw_data {str(image_path)}
+        --raw_data {str(self.image_path)}
         --output_filename_format={str(self.working_dir)}/{{nickname}}_probabilities.tiff ''')
 
         # This will change to submission to slurm
@@ -107,21 +83,15 @@ class CeligoSingleImageCore:
         # and the path to the probability map. This file (filelist.txt) is needed
         # for filelist_path for run_cellprofiler
         with open(self.working_dir / 'filelist.txt', 'w') as rfl:
-            rfl.write(str(image_path))
-            rfl.write(str(image_path.parent / f"{image_path.with_suffix('').with_suffix('').name}_probabilities.tiff"))
+            rfl.write(str(self.image_path))
+            rfl.write(str(self.image_path.parent / f"{self.image_path.with_suffix('').with_suffix('').name}_probabilities.tiff"))
 
-        #returns path to filelist 
-        return self.working_dir / 'filelist.txt'
+        # returns path to filelist 
+        self.filelist = self.working_dir / 'filelist.txt'
 
-    def run_cellprofiler(
-        self,
-        filelist_path: Path
-    ) -> Path:
+    def run_cellprofiler(self) -> Path:
 
         """
-        PARAMETERS: Takes in a path to a filelist.txt file (filelist_path) and an output directory (self.working_dir).
-        Output: Returns a path to the Resized Image 
-        
         FUNCTIONALITY: This method takes aa path to a filelist.txt file and creates a directory with a myriad of
         files that show analytics for the pipeline. It does this by creating a bash script, given above parameters, 
         running said script on the slurm cluster in a pre-existing virtual environment. This is the endpoint of a
@@ -145,7 +115,7 @@ class CeligoSingleImageCore:
         
         # run CellProfiler
         cellprofiler -r -c -p /allen/aics/microscopy/CellProfiler_4.1.3_Testing/96_well_colony_pipeline.cppipe
-        --file-list = {str(filelist_path)} 
+        --file-list = {str(self.filelist_path)} 
         -o {str(self.working_dir / 'cell_profiler_outputs')}''')
 
         #This will change to submission to slurm

c4d062ed40c2b03bc8a6858aa1b1206d0c1e7852
BrianWhitneyAI <brian.whitney@alleninstitute.org>
2022-03-24 13:24:51 -0700

    Added initial orchestration script

--- celigo_pipeline_core/celigo_orchestration.py
+++ celigo_pipeline_core/celigo_orchestration.py
@@ -2,9 +2,8 @@ from multiprocessing.connection import wait
 import os
 from pathlib import Path
 import subprocess
-from tabnanny import check
 
-from celigo_single_image_core import CeligoSingleImageCore
+from . import CeligoSingleImageCore
 
 
 def run_all(raw_image_path):
@@ -21,26 +20,27 @@ def run_all(raw_image_path):
 
     print("Complete")
 
-    def job_complete_check(job_ID,endfile):
-        job_status = 'waiting'
+def job_complete_check(job_ID,endfile):
+    job_status = 'waiting'
 
-        while (not os.path.exists(endfile)) and job_status != 'complete':
-            
-            if  not (job_in_queue_check(job_ID)) and job_status is 'waiting':
-                job_status = 'waiting' # could change to pass
-            elif (job_in_queue_check(job_ID)):
-                job_status = 'running'
-            elif not os.path.exists(endfile):
-                job_status = 'failed'
-                break
-            else:
-                job_status = 'complete'
+    while (not endfile.exists()) and job_status != 'complete':
+        
+        if  not (job_in_queue_check(job_ID)) and job_status is 'waiting':
+            job_status = 'waiting' # could change to pass
+        elif (job_in_queue_check(job_ID)):
+            job_status = 'running'
+        elif not endfile.exists():
+            job_status = 'failed'
+            break
+        else:
+            job_status = 'complete'
+            print("job " + job_ID + " is complete!")
 
-        if job_status is 'failed':
-            x = 0
-            
-    def job_in_queue_check(job_ID):
-         output = subprocess.run(["squeue", "-j", f"{job_ID}"], check=True, capture_output=True) 
-         return output.stdout.decode("utf-8").count("\n") >= 2
+    if job_status is 'failed':
+        print("job " + job_ID + " is Failed")
+
+def job_in_queue_check(job_ID):
+        output = subprocess.run(["squeue", "-j", f"{job_ID}"], check=True, capture_output=True) 
+        return output.stdout.decode("utf-8").count("\n") >= 2
 
         

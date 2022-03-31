d61463d140a096aabb91f412beb3551a3af19110
BrianWhitneyAI <brian.whitney@alleninstitute.org>
2022-03-31 12:38:01 -0700

    testing

--- celigo_pipeline_core/celigo_single_image/celigo_single_image_core.py
+++ celigo_pipeline_core/celigo_single_image/celigo_single_image_core.py
@@ -59,7 +59,7 @@ class CeligoSingleImageCore:
         # Pipeline paths for templates
         with pkg_resources.path(pipelines, "rescale_pipeline.cppipe") as p:
             self.rescale_pipeline_path = p
-        with pkg_resources.path(pipelines, "96_well_colony_pipeline_v_0.1.cppipe") as p:
+        with pkg_resources.path(pipelines, "96_well_colony_pipeline.cppipe") as p:
             self.cellprofiler_pipeline_path = p
         with pkg_resources.path(pipelines, "colony_morphology.model") as p:
             self.classification_model_path = p

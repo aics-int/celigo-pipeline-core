0fa5dfa23de17a4c8931e44da98fb0b5fc1eeff3
BrianWhitneyAI <brian.whitney@alleninstitute.org>
2022-04-01 10:01:39 -0700

    testing

--- setup.py
+++ setup.py
@@ -86,6 +86,6 @@ setup(
     url="https://github.com/aics-int/Celigo-Code-Record",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.2.1",
+    version="1.3.0",
     zip_safe=False,
 )

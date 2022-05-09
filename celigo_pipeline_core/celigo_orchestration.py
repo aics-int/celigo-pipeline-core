e8803d0daee1ccea1f65096a1b2e0b774d82461d
BrianWhitneyAI <brian.whitney@alleninstitute.org>
2022-05-09 13:43:57 -0700

    Changed table name

--- celigo_pipeline_core/celigo_orchestration.py
+++ celigo_pipeline_core/celigo_orchestration.py
@@ -12,7 +12,7 @@ from .celigo_single_image import (
     CeligoSingleImageCore,
 )
 
-TABLE_NAME = '"Celigo_96_Well_Data_Test_V_FOUR"'
+TABLE_NAME = '"Celigo_96_Well_Data_Test_V_FIVE"'
 
 
 def run_all(
